55eac5c0b4185b4d29bb05bc7cc32b7115c61a3a
vikaslohar21 <vikaslohar2122@gmail.com>
2023-10-27 19:05:15 +0530

    Practical-10 Completed

--- app/src/main/java/com/example/mad_practical_10_21012011158/MapsActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011158/MapsActivity.kt
@@ -41,8 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val ahmedabad = LatLng(23.0225, 72.5714)
+        mMap.addMarker(MarkerOptions().position(ahmedabad).title("Marker in Ahmedabad"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ahmedabad))
     }
 }
