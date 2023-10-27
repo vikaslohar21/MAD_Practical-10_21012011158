042a79b30fe4e8a60fb8b6513f17dd85ad52bd3e
vikaslohar21 <vikaslohar2122@gmail.com>
2023-10-27 11:53:05 +0530

    practical-10

--- app/src/main/java/com/example/mad_practical_10_21012011158/ContactAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011158/ContactAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.mad_practical_10_21012011158
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
 import java.text.FieldPosition
 
@@ -15,6 +17,13 @@ class ContactAdapter( context : Context, val array: ArrayList<Contact>) : ArrayA
         view.findViewById<TextView>(R.id.phn_no).text = array.get(position).phoneNo
         view.findViewById<TextView>(R.id.email).text = array.get(position).emailId
         view.findViewById<TextView>(R.id.address).text = array.get(position).address
+
+        val locationButton : Button = view.findViewById(R.id.location)
+
+        locationButton.setOnClickListener(){
+            val intent =  Intent(context,MapsActivity::class.java)
+            context.startActivity(intent)
+        }
         return view
 
 
